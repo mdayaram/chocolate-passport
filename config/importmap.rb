adea4becfe2dab4c7d343fd05ea0e07233948ef8
Manoj Dayaram <m@noj.cc>
2024-05-30 12:49:21 -0700

    feat(lazyload): Only load visible images in the main table

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,4 @@ pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
 # For Chocolate tables
 pin "datatables.net-dt", to: "https://ga.jspm.io/npm:datatables.net-dt@2.0.8/js/dataTables.dataTables.mjs", preload: false
 pin "datatables.net", to: "https://ga.jspm.io/npm:datatables.net@2.0.8/js/dataTables.mjs", preload: false
+pin "lazyload", to: "https://ga.jspm.io/npm:lazyload@2.0.0-rc.2/lazyload.js", preload: false
